67dde576565ab1a4f89dd767b08f41c1d20c4e19
Pamela Rodrigues <junmyeonradpet@gmail.com>
2025-03-23 23:54:07 -0300

    sidebar added

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,29 @@
 import { Header } from './components/Header';
 import { Post } from './Post';
 
-import './style.css';
+import styles from './App.module.css';
+import './global.css';
  
 export function App() {
-// Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae delectus praesentium aliquid culpa soluta, 
-// repellat neque labore ex sapiente officia molestias libero doloribus ea. Optio eius illo at rem eum!
   return (
     <div>
       <Header />
-      <Post 
+      <div className={styles.wrapper}>
+        <Sidebar />
+
+        <main>
+        <Post 
         author="Pamela Rodrigues" 
         content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae delectus praesentium aliquid culpa soluta, " />
-      <Post 
+       <Post 
         author="Daenerys Targaryen" 
         content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae delectus praesentium aliquid culpa soluta, " />
-      <Post 
+        <Post 
         author="John Snow" 
         content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae delectus praesentium aliquid culpa soluta, " />
+        </main>
+
+      </div>
     </div>
   )
 }
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,21 @@
+import styles from './Sidebar.module.css';
+
+export function Sidebar(){
+
+    return (
+        <aside className={styles.sidebar}>
+            <img 
+                className={styles.cover} 
+                    src="https://images.unsplash.com/photo-1488590528505-98d2b5aba04b?fm=jpg&q=50&w=500&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
+
+            <div classname={styles.profile}>
+                <strong>Diego Fernandes</strong>
+                <span>Web Developer</span>
+            </div>
+
+            <footer>
+                <a href="#">Editar seu perfil</a>
+            </footer>
+        </aside>
+    );
+}
