cc4e26c75d18832ebc3dc60eb3d0ad4e0f527b78
Pamela Rodrigues <junmyeonradpet@gmail.com>
2025-03-23 21:37:07 -0300

    c2

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
-function App() {
+import { Post } from './Post';
 
+import './style.css';
+ 
+export function App() {
+// Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae delectus praesentium aliquid culpa soluta, 
+// repellat neque labore ex sapiente officia molestias libero doloribus ea. Optio eius illo at rem eum!
   return (
-    <>
-    <h1>Hello, World!</h1>
-    </>
+    <div>
+      <Post 
+        author="Pamela Rodrigues" 
+        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae delectus praesentium aliquid culpa soluta, " />
+      <Post 
+        author="Daenerys Targaryen" 
+        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae delectus praesentium aliquid culpa soluta, " />
+      <Post 
+        author="Sir Jorha Mormont" 
+        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae delectus praesentium aliquid culpa soluta, " />
+    </div>
   )
 }
-
-export default App
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App'
+import { App } from './App'
+
+console.log("O React está rodando!");
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
